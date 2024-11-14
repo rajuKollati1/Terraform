440a33457efb11d59eea33e294a207cb8326a295
rajuKollati1 <130030878+rajuKollati1@users.noreply.github.com>
2024-11-14 13:45:06 +0530

    Update main.tf

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -1,29 +1,21 @@
-variable "max_pods_per_node" {
-  description = "Maximum pods per node"
-  type        = number
-}
-
 resource "google_container_cluster" "primary" {
-  name               = var.cluster_name
-  location           = var.region
-  remove_default_node_pool = true
-  initial_node_count = 1
+  name     = var.cluster_name
+  location = var.region
 
-  # Other necessary cluster configurations
+  remove_default_node_pool = true
+  initial_node_count       = 1
 }
 
 resource "google_container_node_pool" "primary_nodes" {
-  cluster    = google_container_cluster.primary.name
-  location   = var.region
+  cluster  = google_container_cluster.primary.name
+  location = var.region
   node_count = var.num_nodes
 
   node_config {
-    machine_type             = var.machine_type
-    labels                   = { namespace = var.node_label }
-    oauth_scopes             = ["https://www.googleapis.com/auth/cloud-platform"]
-
-    # Use max_pods_per_node here
-    default_max_pods_per_node = var.max_pods_per_node
+    machine_type          = var.machine_type
+    labels                = { namespace = var.node_label }
+    oauth_scopes          = ["https://www.googleapis.com/auth/cloud-platform"]
+    max_pods_per_node     = var.max_pods_per_node
   }
 }
 
