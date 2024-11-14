0f71cc6fa0eca8271f02119d606a3b99d9b48cbc
rajuKollati1 <130030878+rajuKollati1@users.noreply.github.com>
2024-11-14 13:11:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,12 +7,15 @@ module "vpc" {
   source       = "./modules/vpc"
   vpc_name     = var.vpc_name
   subnet_name  = var.subnet_name
+  network_range = var.network_range
+  region       = var.region
 }
 
 module "region" {
-  source      = "./modules/region"
-  project_id  = var.project_id
-  region      = var.region
+  source       = "./modules/region"
+  project_id   = var.project_id
+  region       = var.region
+  compute_zone = var.compute_zone
 }
 
 module "cluster" {
@@ -22,5 +25,5 @@ module "cluster" {
   machine_type       = var.machine_type
   node_label         = var.node_label
   max_pods_per_node  = var.max_pods_per_node
-  region             = var.region 
+  region             = var.region       # Add region here
 }
