6c4ca639721f84b409bfb61629d24a58dde4c4ad
rajuKollati1 <130030878+rajuKollati1@users.noreply.github.com>
2024-11-14 12:51:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,28 +1,9 @@
 variable "project_id" {}
-variable "region" {
-  default = "asia-east1"
-}
-variable "compute_zone" {
-  default = "asia-east1-a"
-}
-variable "vpc_name" {
-  default = "my-vpc001"
-}
-variable "subnet_name" {
-  default = "my-vpc001"
-}
-variable "cluster_name" {
-  default = "rk-cluster-01"
-}
-variable "num_nodes" {
-  default = 2
-}
-variable "machine_type" {
-  default = "e2-medium"
-}
-variable "node_label" {
-  default = "namespace=rk-lable-node-01"
-}
-variable "max_pods_per_node" {
-  default = 80
-}
+variable "region" {}
+variable "vpc_name" {}
+variable "subnet_name" {}
+variable "cluster_name" {}
+variable "num_nodes" {}
+variable "machine_type" {}
+variable "node_label" {}
+variable "max_pods_per_node" {}
