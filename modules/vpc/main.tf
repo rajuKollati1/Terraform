8abc28b41a746edf8cc1e86507ccc25dda957b55
rajuKollati1 <130030878+rajuKollati1@users.noreply.github.com>
2024-11-14 12:57:01 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,3 +1,10 @@
 resource "google_compute_network" "vpc_network" {
   name = var.vpc_name
 }
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = var.subnet_name
+  ip_cidr_range = var.network_range
+  region        = var.region
+  network       = google_compute_network.vpc_network.self_link
+}
