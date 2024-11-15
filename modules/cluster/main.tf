652e0969fc4208f27d92cb66f4afb666e03fb7a5
rajuKollati1 <130030878+rajuKollati1@users.noreply.github.com>
2024-11-15 20:08:16 +0530

    Update main.tf

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -4,18 +4,27 @@ resource "google_container_cluster" "primary" {
 
   remove_default_node_pool = true
   initial_node_count       = 1
+
+  ip_allocation_policy {
+    use_ip_aliases = true
+  }
 }
 
 resource "google_container_node_pool" "primary_nodes" {
-  cluster  = google_container_cluster.primary.name
-  location = var.region
+  cluster    = google_container_cluster.primary.name
+  location   = var.region
   node_count = var.num_nodes
 
   node_config {
-    machine_type          = var.machine_type
-    labels                = { namespace = var.node_label }
-    oauth_scopes          = ["https://www.googleapis.com/auth/cloud-platform"]
-    max_pods_per_node     = var.max_pods_per_node
+    machine_type = var.machine_type
+    labels       = { namespace = var.node_label }
+    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+  # Optional: Autoscaling
+  autoscaling {
+    min_node_count = 1
+    max_node_count = var.num_nodes * 2
   }
 }
 
