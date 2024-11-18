de3197b525a29965347fb32c1be1d6a6e85514e0
rajuKollati1 <130030878+rajuKollati1@users.noreply.github.com>
2024-11-18 10:54:36 +0530

    Update main.tf

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -5,6 +5,14 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
   initial_node_count       = 1
 
+  # Specify the VPC and Subnet
+  network    = "projects/halogen-obelisk-441705-u4/global/networks/rk-vpc"   # Replace 'myvpc' with your VPC name
+  subnetwork = "projects/halogen-obelisk-441705-u4/regions/${var.region}/subnetworks/rk-sub" # Replace 'mysubnet' with your Subnet name
+
+  ip_allocation_policy {
+    cluster_secondary_range_name  = var.cluster_secondary_range  # Replace with the secondary range for Pods
+    services_secondary_range_name = var.services_secondary_range # Replace with the secondary range for Services
+  }
 }
 
 resource "google_container_node_pool" "primary_nodes" {
